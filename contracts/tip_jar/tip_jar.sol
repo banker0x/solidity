0ee9cc1404714c358d4bbe4a3e4fdc9b4079f07d
John Matsikas <john.matsikas@gmail.com>
2021-08-27 10:10:38 -0600

    sending/receiving ETH from contract

---
+++ contracts/tip_jar/tip_jar.sol
@@ -0,0 +1,27 @@
+pragma solidity 0.5.16;
+
+contract TipJar {
+
+    address owner;    // current owner of the contract
+
+    function TipJar() public {
+        owner = msg.sender;
+    }
+
+    function withdraw() public {
+        require(owner == msg.sender);
+
+        // Transfer the contract balance back to the owner
+        msg.sender.transfer(address(this).balance);
+    }
+
+    // As a payable function, $ETH is being sent to this contract
+    function deposit(uint256 amount) public payable {
+        require(msg.value == amount);
+    }
+
+    // Retrive contract balance
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+}
