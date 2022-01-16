7cc5fa8c15f9cb5b1c344c2972e7796ecec40479
matsikas <65791382+matsikas@users.noreply.github.com>
2022-01-16 09:26:54 -0700

    Update Library.sol

--- contracts/library/Library.sol
+++ contracts/library/Library.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.3;
 
+/* 
+A library is a different type of contract, that doesn't have any storage and CANNOT hold ether. 
+Sometimes it is helpful to think of a library as a singleton in the EVM, a piece of code that can be 
+called from any contract without the need to deploy it again.
+*/
+
 library Math {
 
     // Pure functions do not read from storage
