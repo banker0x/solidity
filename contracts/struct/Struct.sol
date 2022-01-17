909381047bb2929b7098390cc2508bff778f45d4
matsikas <65791382+matsikas@users.noreply.github.com>
2022-01-16 21:22:31 -0700

    Create Struct.sol

---
+++ contracts/struct/Struct.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract Todos {
+
+    struct Todo {
+        string text;
+        bool completed;
+    }
+
+    // An array of 'Todo' structs
+    Todo[] public todos;
+
+    function create(string memory _text) public {
+        // 3 ways to initialize a struct
+        // - calling it like a function
+        todos.push(Todo(_text, false));
+
+        // key value mapping
+        todos.push(Todo({text: _text, completed: false}));
+
+        // initialize an empty struct and then update it
+        Todo memory todo;
+        todo.text = _text;
+        // todo.completed initialized to false
+
+        todos.push(todo);
+    }
+
+    // update text
+    function update(uint _index, string memory _text) public {
+        Todo storage todo = todos[_index];
+        todo.text = _text;
+    }
+}
