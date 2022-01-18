8fde542365d95bbbb24498139d7ec0d4d9aaed25
matsikas <65791382+matsikas@users.noreply.github.com>
2022-01-18 10:37:56 -0700

    Create Interface.sol

---
+++ contracts/interface/Interface.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+/*
+    Interface:
+
+        Cannot have any functions implemented
+        Can inherit from other interfaces
+        All declared functions must be external
+        Cannot declare a constructor
+        Cannot declare state variables
+*/
+
+contract Counter {
+    uint public count;
+
+    function increment() external {
+        count += 1;
+    }
+}
+
+interface ICounter {
+    function count() external view returns (uint);
+
+    function increment() external;
+}
+
+contract MyContract {
+    function incrementCounter(address _counter) external {
+        ICounter(_counter).increment();
+    }
+
+    function getCount(address _counter) external view returns (uint) {
+        return ICounter(_counter).count();
+    }
+}
